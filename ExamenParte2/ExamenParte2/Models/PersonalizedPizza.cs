6e6ba811b518aef51d688be5e9fac03a6a8e8e50
DavidXie10 <davidxieli@gmail.com>
2021-11-28 17:17:08 -0600

    Crear pizza: Creación del modelo de la pizza personalizada

---
+++ ExamenParte2/ExamenParte2/Models/PersonalizedPizza.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenParte2.Models {
+    public class PersonalizedPizza {
+        [Required(ErrorMessage = "Es necesario que seleccione un tamaño")]
+        [Display(Name = "Seleccione el tamaño")]
+        public string Size { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que seleccione un tipo de masa")]
+        [Display(Name = "Seleccione la masa")]
+        public string Mass { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que seleccione una opción")]
+        [Display(Name = "¿Cuál es su salsa?")]
+        public string Sauce { get; set; }
+
+        [Required(ErrorMessage = "Es necesario que seleccione una opción")]
+        [Display(Name = "¿Cuánta mozzarella le apetece?")]
+        public string Mozzarella { get; set; }
+    }
+}
