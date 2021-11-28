20fdea9c8de26bfc35a3e00c80edb044029d8772
DavidXie10 <davidxieli@gmail.com>
2021-11-28 17:19:53 -0600

    Crear pizza: Implementación de handler genérico de la base de datos

---
+++ ExamenParte2/ExamenParte2/Handlers/DatabaseHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ExamenParte2.Handlers {
+    public class DatabaseHandler {
+        protected SqlConnection connection;
+        protected string connectionRoute;
+        protected const string CONNECTION_NAME = "ExamenConnection";
+
+        public DatabaseHandler() {
+            connectionRoute = ConfigurationManager.ConnectionStrings[CONNECTION_NAME].ToString();
+            connection = new SqlConnection(connectionRoute);
+        }
+
+        public DataTable CreateTableFromQuery(string query) {
+            SqlCommand queryCommand = new SqlCommand(query, connection);
+            SqlDataAdapter tableAdapter = new SqlDataAdapter(queryCommand);
+            DataTable queryTable = new DataTable();
+            connection.Open();
+            tableAdapter.Fill(queryTable);
+            connection.Close();
+            return queryTable;
+        }
+
+        public bool DatabaseQuery(string query) {
+            bool success = false;
+            SqlCommand queryCommand = new SqlCommand(query, connection);
+            connection.Open();
+            success = queryCommand.ExecuteNonQuery() >= 1;
+            connection.Close();
+            return success;
+        }
+
+        public bool DatabaseQuery(SqlCommand queryCommand) {
+            bool success = false;
+            connection.Open();
+            success = queryCommand.ExecuteNonQuery() >= 1;
+            connection.Close();
+            return success;
+        }
+    }
+}
