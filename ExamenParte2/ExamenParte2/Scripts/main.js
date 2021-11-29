b710797f0f2ea926e64814469f4194d815b3cf4d
DavidXie10 <davidxieli@gmail.com>
2021-11-28 21:29:49 -0600

    Crear pizza: Implementación de script para dinamismo en ingredientes

---
+++ ExamenParte2/ExamenParte2/Scripts/main.js
@@ -0,0 +1,21 @@
+﻿function increment(inputId) {
+    let input = document.getElementById(inputId);
+    input.value = parseInt(input.value) + 1;
+    console.log(input.value);
+}
+
+function decrement(inputId) {
+    let input = document.getElementById(inputId);
+    if (parseInt(input.value) - 1 >= 0) {
+        input.value = input.value - 1;
+    }
+    console.log(input.value);
+}
+
+function hide(elementId) {
+    document.getElementById(elementId).style.display = "none";
+}
+
+function show(elementId) {
+    document.getElementById(elementId).style.display = "block";
+}
