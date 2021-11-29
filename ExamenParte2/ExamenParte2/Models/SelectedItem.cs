439d5cd043ad6e399ad95a642660534a2eedfb43
DavidXie10 <davidxieli@gmail.com>
2021-11-28 21:27:09 -0600

    Crear pizza: Implementación del modelo de un item seleccionado

---
+++ ExamenParte2/ExamenParte2/Models/SelectedItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenParte2.Models {
+    public class SelectedItem {
+        public string Description { get; set; }
+
+        public int Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int Total { get; set; }
+
+        public void CalculateTotal (){
+            this.Total = this.Quantity * this.Price;
+        }
+    }
+}
