c3a65a18fcde259644635e513d2a312085a8a553
DavidXie10 <davidxieli@gmail.com>
2021-11-29 20:22:31 -0600

    Calcular precio: Creación de modelo para dar forma de retiro

---
+++ ExamenParte2/ExamenParte2/Models/PickOrderInformation.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenParte2.Models {
+    public class PickOrderInformation {
+        [Required(ErrorMessage = "Es necesario que seleccione la forma de retiro")]
+        public string PickOrderChoice { get; set; }
+    }
+}
