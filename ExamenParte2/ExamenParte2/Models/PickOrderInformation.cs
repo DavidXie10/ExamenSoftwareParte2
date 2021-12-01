1f385ee490876013570636fb197fc6db08bc59a7
DavidXie10 <davidxieli@gmail.com>
2021-11-30 21:46:59 -0600

    Forma de retiro: Adaptado el modelo para retiro de orden

--- ExamenParte2/ExamenParte2/Models/PickOrderInformation.cs
+++ ExamenParte2/ExamenParte2/Models/PickOrderInformation.cs
@@ -4,5 +4,19 @@ namespace ExamenParte2.Models {
     public class PickOrderInformation {
         [Required(ErrorMessage = "Es necesario que seleccione la forma de retiro")]
         public string PickOrderChoice { get; set; }
+
+        [Display(Name = "Dirección exacta")]
+        [Required(ErrorMessage = "Es necesario que llene el campo")]
+        public string ExactDirection { get; set; }
+
+        public string Province { get; set; }
+
+        public string Canton { get; set; }
+
+        public string District { get; set; }
+
+        public int Total { get; set; }
+
+        public string ProductName { get; set; }
     }
 }
