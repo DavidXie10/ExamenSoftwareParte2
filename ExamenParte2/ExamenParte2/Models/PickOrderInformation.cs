384ab93c396bb7e45f79dbaac2fd5d7b1776b647
DavidXie10 <davidxieli@gmail.com>
2021-12-01 00:24:51 -0600

    Forma de retiro: Modelo de forma de retiro adaptado a la entrega a domicilio

--- ExamenParte2/ExamenParte2/Models/PickOrderInformation.cs
+++ ExamenParte2/ExamenParte2/Models/PickOrderInformation.cs
@@ -9,13 +9,24 @@ namespace ExamenParte2.Models {
         [Required(ErrorMessage = "Es necesario que llene el campo")]
         public string ExactDirection { get; set; }
 
+        [Display(Name = "Provincia")]
+        [Required(ErrorMessage = "Es necesario que seleccione una provincia")]
         public string Province { get; set; }
 
+        [Display(Name = "Cantón")]
+        [Required(ErrorMessage = "Es necesario que ingrese el cantón")]
         public string Canton { get; set; }
 
+        [Display(Name = "Distrito")]
+        [Required(ErrorMessage = "Es necesario que ingrese el distrito")]
         public string District { get; set; }
 
-        public int Total { get; set; }
+        [Display(Name = "Teléfono")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Debe ingresar números")]
+        [Required(ErrorMessage = "Es necesario que ingrese su número de teléfono")]
+        public int ContactNumber { get; set; }
+
+        public string Total { get; set; }
 
         public string ProductName { get; set; }
     }
