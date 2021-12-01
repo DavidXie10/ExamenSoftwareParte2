f832c213acad427180d8c816b870c68de9587f06
DavidXie10 <davidxieli@gmail.com>
2021-12-01 00:25:38 -0600

    Forma de retiro: Adaptado el controlador a la entrega a domicilio

--- ExamenParte2/ExamenParte2/Controllers/PickUpOrderController.cs
+++ ExamenParte2/ExamenParte2/Controllers/PickUpOrderController.cs
@@ -2,23 +2,30 @@
 using ExamenParte2.Models;
 using System.Web.Mvc;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace ExamenParte2.Controllers{
     public class PickUpOrderController : Controller {
         private string[] LOCATIONS = { "Pizza Hut Guacima (Plaza Castillo)", "Pizza Hut Tibás (Centro comercial Plaza del Valle)", "Pizza Hut Escazú (Frente Perimercado Los Anonos)", "Pizza Hut Paseo Colón (Frente al Hotel Ambásador sobre el Paseo Colón)", "Pizza Hut Guadalupe (Centro Comercial Guadalupe)", "Pizza Hut Sabanilla (Sabanilla, Centro Comercial Vistana)", "Pizza Hut San Ramón (San Ramón Centro, Alajuela)" };
+        private string[] PROVINCES = { "SAN JOSÉ", "ALAJUELA", "CARTAGO", "HEREDIA", "GUANACASTE", "PUNTARENAS", "LIMÓN" };
 
         [HttpPost]
         public ActionResult SubmitPickUpOrderChoice(PickOrderInformation pickUpChoice) {
-            string choice = pickUpChoice.PickOrderChoice;
-            string controllerMethod = choice == "Express" ? "Express" : "InRestaurant";
-            string productName = Request.Form["productName"];
-            int total = int.Parse(Request.Form["totalHidden"]);
+            ActionResult view = RedirectToAction("Index", "Home");
+            //try {
+                string choice = pickUpChoice.PickOrderChoice;
+                string controllerMethod = choice == "Express" ? "Express" : "InRestaurant";
+                string productName = Request.Form["productName"];
+                string totalHidden = Request.Form["totalHidden"];
 
-            pickUpChoice.ProductName = productName;
-            pickUpChoice.Total = total;
+                pickUpChoice.ProductName = productName;
+                pickUpChoice.Total = totalHidden;
 
-            TempData["pickUpInformation"] = pickUpChoice;
-            ActionResult view = RedirectToAction(controllerMethod, "PickUpOrder");
+                TempData["pickUpInformation"] = pickUpChoice;
+                view = RedirectToAction(controllerMethod, "PickUpOrder");
+           // } catch (Exception exception) {
+             //   Debug.WriteLine(exception.ToString());
+            //}
 
             return view;
         }
@@ -40,31 +47,32 @@ namespace ExamenParte2.Controllers{
             return View();
         }
 
-        [HttpPost]
-        public ActionResult SubmitInRestaurantChoice(PickOrderInformation pickUpChoice) {
-            string productName = Request.Form["productName"];
-            int total = Convert.ToInt32(Request.Form["totalHidden"]);
-
-            pickUpChoice.Total = total;
-            pickUpChoice.ProductName = productName;
-
-            TempData["pickUpInformation"] = pickUpChoice;
-
-            ActionResult view = RedirectToAction("Invoice", "PickUpOrder");
-
-            return view;
-        }
-
         public ActionResult Express() {
             ViewBag.PickUpModel = TempData["pickUpInformation"] as PickOrderInformation;
+            ViewBag.Provinces = GetDropdown(new List<string>(PROVINCES));
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult SubmitExpressChoice(PickOrderInformation pickUpChoice) {
+        public ActionResult SubmitPickUpInformation(PickOrderInformation pickUpChoice) {
+            ActionResult view = RedirectToAction("Index", "Home");
 
-            return View();
+            try {
+                string productName = Request.Form["productName"];
+                string total = Request.Form["totalHidden"];
+
+                pickUpChoice.Total = total;
+                pickUpChoice.ProductName = productName;
+
+                TempData["pickUpInformation"] = pickUpChoice;
+
+                view = RedirectToAction("Invoice", "PickUpOrder");
+            } catch (Exception exception) {
+                Debug.WriteLine(exception.ToString());
+            }
+
+            return view;
         }
 
         public ActionResult Invoice() {
